48ef80421bb838f1136d96ba684a79e6fc70f49f
b.slaveykov <b.slaveykov@radioactive.bg>
2019-10-04 18:41:36 +0300

    update

--- plib/library/Install.php
+++ plib/library/Install.php
@@ -63,7 +63,7 @@ protected $_logger;
 	    
 	// Add SSL
 	try {
-		$this->_logger->write('SSL for domain: ' . $domain->getName() . '; Email: ' . $sslEmail;
+		$this->_logger->write('SSL for domain: ' . $domain->getName() . '; Email: ' . $sslEmail);
 		$artisan = pm_ApiCli::callSbin('encrypt_domain.sh', [$domain->getName(), $sslEmail]);
 		$this->_logger->write('Encrypt domain log for: ' . $domain->getName() . '<br />' . $artisan['stdout']. '<br /><br />');
 		$this->_logger->write('Success instalation SSL for domain: ' . $domain->getName());
